01347ce10e8661392c73daf7a18c4ff723761908
Bivek <beevk.singh@gmail.com>
2024-06-01 01:17:42 +0700

    fix: update config required for Cloudflare pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+
+const nextConfig = {
+    runtime: 'edge',
+    unstable_allowDynamic: [
+        // allows a single file
+        '/lib/utilities.js',
+        // use a glob to allow anything in the function-bind 3rd party module
+        '/node_modules/function-bind/**',
+    ],
+};
 
 export default nextConfig;
